1ced8d328177162a68fab155ec7ac508e1cda195
toozenbobel <xseder@gmail.com>
2014-07-27 00:05:31 +0400

    Added missing file:<F12>

---
+++ tvmanager/HostTool/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HostTool.Tools;
+
+namespace HostTool
+{
+	
+
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void HostClick(object sender, RoutedEventArgs e)
+		{
+			ServicesHostingManager manager = new ServicesHostingManager();
+			manager.StartServices();
+		}
+	}
+}
